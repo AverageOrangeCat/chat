81704e2b4fe45de20627271040991f46a90232dc
KuengNoel <noel.kueng@ik.me>
2024-09-25 13:42:49 +0200

    Delete legacy project

--- legacy_test_project/service/infrastructure/src/main/java/org/chat/infrastructure/Infrastructure.java
+++
@@ -1,8 +0,0 @@
-package org.chat.infrastructure;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class Infrastructure {
-
-}
--- legacy_test_project/service/shared/src/main/java/org/chat/shared/Shared.java
+++
@@ -1,8 +0,0 @@
-package org.chat.shared;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class Shared {
-
-}
